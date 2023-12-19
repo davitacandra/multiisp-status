af45831ab8bbb662052fb2c0a42ce516a45cb2c1
davita <davita@nusa.net.id>
2023-12-19 14:24:26 +0700

    Filtering by timestamp

--- src/main.ts
+++ src/main.ts
@@ -4,46 +4,70 @@ const app = express()
 app.use(express.json())
 
 // Define the data structure
-interface Data {
+interface Status {
   isp: string
   status: number
+  timestamp: number
+}
+
+interface Data {
+    statuses : Status[]
 }
 
 // Define the type for dataStore
 const dataStore: Record<string, Data> = {}
 
+// Set an interval to clear the dataStore every 5 minutes
+// const interval = setInterval(() => {
+//     console.log("Clearing dataStore")
+//     for (const key in dataStore) {
+//       delete dataStore[key]
+//     }
+//   }, 300000) // 300000 milliseconds = 5 minutes
+
 app.post('/data', (req, res) => {
   const { node, isp, status } = req.body
-
-  dataStore[node] = { isp, status }
-
-  res.send(`Data recieved for node ${node}`)
-})
-
-app.get('/data/:node', (req, res) => {
-  const node = req.params.node
-  const data = dataStore[node]
+  const timestamp = Date.now()
   
-  if(data){
-    const formattedData = `multiisp_status{node="${node},"isp="${data.isp}"} ${data.status}`
-    res.send(formattedData)
-  } else {
-    res.status(404).send('Data not found')
+  if (!dataStore[node]) {
+    dataStore[node] = { statuses: [] }
   }
+  dataStore[node].statuses.push({ isp, status, timestamp })
+  
+  res.send(`Data received for node ${node}`)
 })
+
+app.get('/data', (req, res) => {
+  let formattedResponse = ''
+  const currentTime = Date.now()
+  for (const node in dataStore) {
+    dataStore[node].statuses = dataStore[node].statuses.filter(status =>
+      currentTime - status.timestamp < 300000    
+    )
+    
+    dataStore[node].statuses.forEach(status => {
+      formattedResponse += `multiisp_status{node="${node}",isp="${status.isp}"} ${status.status}\n`
+    })
+  }
+  
+  res.type('text').send(formattedResponse.trim())
+})
+
+// app.get('/data/:node', (req, res) => {
+//   const node = req.params.node
+//   const data = dataStore[node]
+  
+//   if(data){
+//     const formattedData = `multiisp_status{node="${node},"isp="${data.isp}"} ${data.status}`
+//     res.send(formattedData)
+//   } else {
+//     res.status(404).send('Data not found')
+//   }
+// })
 
 // app.get('/data', (req, res) => {
 //   res.json(dataStore)
 // })
-
-app.get('/data', (req, res) => {
-  let formattedResponse = ''
-  for (const node in dataStore) {
-    const data = dataStore[node]
-    formattedResponse += `multiisp_status{node="${node}",isp="${data.isp}"} ${data.status}\n`
-  }
-  res.type('text').send(formattedResponse.trim())
-})
 
 const PORT = 3002
 app.listen(PORT, () => {
